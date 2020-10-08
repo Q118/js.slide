510e49d4330c650dbc441af48b961a6235c5f7c5
Q118 <shelbyfish91@gmail.com>
2020-10-08 10:40:43 -0700

    finished functionality of inputs with propertiesupdating

--- script.js
+++ script.js
@@ -2,5 +2,14 @@
 const inputs = document.querySelectorAll('.controls input');
 
 function handleUpdate() {
-    console.log(this.value);
+    const suffix = this.dataset.sizing || '';
+    document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
 }
+
+//able to use forEach on nodelist items
+inputs.forEach(input => input.addEventListener('change', handleUpdate));
+
+//when value is moved-also triggering a change
+inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+
+//data set is an object that will contain all the data attributes from that specific element
